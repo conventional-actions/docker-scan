0f3cd622fe7c152f0196508f672496ca98eb3685
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-08-24 00:33:34 -0700

    feat: initial version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,26 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_EXCLUDE_BASE'] = 'true'
+  process.env['INPUT_IMAGE'] = 'github/actions'
+  process.env['INPUT_TAG'] = 'latest'
+  process.env['INPUT_SEVERITY'] = 'high'
+  process.env['INPUT_FILE'] = 'docker/alpine/Dockerfile'
+  process.env['INPUT_TOKEN'] = 'lkjdfkjdsklf'
+
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    env: process.env,
+    stdio: 'pipe'
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  try {
+    console.log(cp.execFileSync(np, [ip], options).toString())
+  } catch (error) {
+    console.error(error)
+  }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,66 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const images: string[] = (core.getInput('image') || '').split(/\r?\n/)
+    const tags: string[] = (core.getInput('tag') || 'latest').split(/\r?\n/)
+    const severity: string = core.getInput('severity') || 'medium'
+    const file: string = core.getInput('file') || 'Dockerfile'
+    const token: string =
+      core.getInput('token') || process.env['SNYK_AUTH_TOKEN'] || ''
+    const excludeBase: boolean =
+      (core.getInput('excludeBase') || process.env['DOCKER_EXCLUDE_BASE']) ===
+      'true'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(core.getInput('exclude-base'))
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!images) {
+      core.error('image input is required')
+      return
+    }
+
+    if (!token) {
+      core.error('image token or env SNYK_AUTH_TOKEN is required')
+      return
+    }
+
+    let args = [
+      'scan',
+      '--accept-license',
+      '--dependency-tree',
+      '--file',
+      file,
+      '--severity',
+      severity
+    ]
+
+    if (excludeBase) {
+      args = args.concat('--exclude-base')
+    }
+
+    if (token) {
+      core.setSecret(token)
+      args = args.concat('--token', token)
+    }
+
+    const options: ExecOptions = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          core.info(data.toString())
+        },
+        stderr: (data: Buffer) => {
+          core.error(data.toString())
+        }
+      }
+    }
+
+    images.map(async image =>
+      tags.map(async tag =>
+        exec.exec('docker', args.concat(`${image}:${tag}`), options)
+      )
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
